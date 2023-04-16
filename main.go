1dcaadc19c2677e688c239387ab28636d3be0587
Sarthak Dubey <sarthak.dubey123@gmail.com>
2023-04-16 13:38:33 -0700

    Completing module 1

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
+import "CourseraGo/assesments"
+
 func main() {
-	//assesments.Hello()
-	//assesments.Trunc()
-	//assesments.Findian()
-	//assesments.Slice()
-	//assesments.MakeJson()
-	//assesments.ReadFileFirstLastNameFile()
+	assesments.Hello()
+	assesments.Trunc()
+	assesments.Findian()
+	assesments.Slice()
+	assesments.MakeJson()
+	assesments.ReadFileFirstLastNameFile()
 }
